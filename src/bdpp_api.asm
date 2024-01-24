27959421d4e2bd74840871c17048d3b76db854e3
Curtis Whitley <cwhitley@pesa.com>
2024-01-24 16:21:06 -0500

    comment fcn codes

--- src/bdpp_api.asm
+++ src/bdpp_api.asm
@@ -20,21 +20,21 @@
 			XREF	GET_AHL24
 			XREF	SET_ADE24
 
-			XREF	_bdpp_is_allowed
-			XREF	_bdpp_is_enabled
-			XREF	_bdpp_enable
-			XREF	_bdpp_disable
-			XREF	_bdpp_queue_tx_app_packet
-			XREF	_bdpp_prepare_rx_app_packet
-			XREF	_bdpp_is_tx_app_packet_done
-			XREF	_bdpp_is_rx_app_packet_done
-			XREF	_bdpp_get_rx_app_packet_flags
-			XREF	_bdpp_get_rx_app_packet_size
-			XREF	_bdpp_stop_using_app_packet 
-			XREF	_bdpp_write_byte_to_drv_tx_packet
-			XREF	_bdpp_write_bytes_to_drv_tx_packet
-			XREF	_bdpp_write_drv_tx_data_with_usage
-			XREF	_bdpp_flush_drv_tx_packet
+			XREF	_bdpp_is_allowed					; 0x00
+			XREF	_bdpp_is_enabled					; 0x01
+			XREF	_bdpp_enable						; 0x02
+			XREF	_bdpp_disable						; 0x03
+			XREF	_bdpp_queue_tx_app_packet			; 0x04
+			XREF	_bdpp_prepare_rx_app_packet			; 0x05
+			XREF	_bdpp_is_tx_app_packet_done			; 0x06
+			XREF	_bdpp_is_rx_app_packet_done			; 0x07
+			XREF	_bdpp_get_rx_app_packet_flags		; 0x08
+			XREF	_bdpp_get_rx_app_packet_size		; 0x09
+			XREF	_bdpp_stop_using_app_packet 		; 0x0A
+			XREF	_bdpp_write_byte_to_drv_tx_packet	; 0x0B
+			XREF	_bdpp_write_bytes_to_drv_tx_packet	; 0x0C
+			XREF	_bdpp_write_drv_tx_data_with_usage	; 0x0D
+			XREF	_bdpp_flush_drv_tx_packet			; 0x0E
 
 ; Call a BDPP API function
 ;  A: function to call
