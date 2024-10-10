3b5099a232d3dff010ac67295d006981ba963a63
Tom Morton <tomm@riseup.net>
2024-10-10 22:34:42 +0100

    Version bump to 2.3.0

--- src/version.h
+++ src/version.h
@@ -4,8 +4,8 @@
 #define		VERSION_MAJOR		2
 #define		VERSION_MINOR		3
 #define		VERSION_PATCH		0
-#define		VERSION_CANDIDATE	1			// Optional
-#define		VERSION_TYPE		"beta-"	// RC, Alpha, Beta, etc.
+#define		VERSION_CANDIDATE	0			// Optional
+//#define		VERSION_TYPE		"beta-"	// RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Rainbow"
 //#define     VERSION_SUBTITLE    "Rainbow"
