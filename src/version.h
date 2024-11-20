5259f31f1aab580748409e2aa40af6da9587a70c
Steve Sims <steve@stevesims.dev>
2024-11-20 16:39:24 +0000

    tweak version

--- src/version.h
+++ src/version.h
@@ -1,11 +1,11 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define		VERSION_MAJOR		2
-#define		VERSION_MINOR		3
-#define		VERSION_PATCH		1
-// #define		VERSION_CANDIDATE	1			// Optional
-// #define		VERSION_TYPE		"Test "	// RC, Alpha, Beta, etc.
+#define		VERSION_MAJOR		3
+#define		VERSION_MINOR		0
+#define		VERSION_PATCH		0
+#define		VERSION_CANDIDATE	1			// Optional
+#define		VERSION_TYPE		"Alpha "	// RC, Alpha, Beta, etc.
 
 #define		VERSION_VARIANT		"Console8"
 #define     VERSION_SUBTITLE    "Rainbow"
